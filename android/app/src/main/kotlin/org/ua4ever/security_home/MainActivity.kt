33974a9eed6861b6a0a4cfc4a95bec8497878567
hex4coder <the.programmer.luyo@gmail.com>
2021-05-17 17:33:45 +0800

    init

---
+++ android/app/src/main/kotlin/org/ua4ever/security_home/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.ua4ever.security_home
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:security_home/pages/splash_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
